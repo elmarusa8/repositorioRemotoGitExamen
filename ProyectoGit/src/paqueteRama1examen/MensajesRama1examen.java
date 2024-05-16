45bcd8de4e572faa9bd54a1695ebe657bb49f782
elmar <elmar@Kariflain>
2024-05-16 18:13:11 +0100

    a

---
+++ ProyectoGit/src/paqueteRama1examen/MensajesRama1examen.java
@@ -0,0 +1,11 @@
+package paqueteRama1examen;
+
+public class MensajesRama1examen {
+
+	public static void main(String[] args) {
+		System.out.println("Mensaje 1");
+		System.out.println("Mensaje 2");
+
+	}
+
+}
